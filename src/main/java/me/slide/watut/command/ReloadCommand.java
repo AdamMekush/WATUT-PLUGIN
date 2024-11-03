e044f3497a9325696cbf5df1e3a892f6c9f6f4d7
Adam Mekush <60749996+AdamMekush@users.noreply.github.com>
2024-11-03 05:02:59 +0100

    Small fix for ReloadCommand

--- src/main/java/me/slide/watut/command/ReloadCommand.java
+++ src/main/java/me/slide/watut/command/ReloadCommand.java
@@ -10,6 +10,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ReloadCommand implements CommandExecutor, TabCompleter {
@@ -17,14 +18,26 @@ public class ReloadCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String name, @NotNull String[] strings) {
         if(name.equals("watut")){
+
+            if (Arrays.stream(strings).toList().isEmpty()) {
+                commandSender.sendMessage("§cUsage: /watut reload");
+                return true;
+            }
+
             if(strings[0].equals("reload")){
+                if (!commandSender.hasPermission("watut.reload")) {
+                    commandSender.sendMessage("§cYou do not have permission to execute this command.");
+                    return true;
+                }
+
                 Config config = Config.deserialize(WatutPlugin.getInstance().getConfig().getValues(true));
                 WatutPlugin.getInstance().setConfiguration(config);
                 WatutPlugin.getInstance().reloadConfig();
                 commandSender.sendMessage("§2Configuration successfully reloaded!");
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     @Override
